098dd5ebb89d0abfc62842d01bcd426df120c542
Ali Elgamal <alimelgamal199@gmail.com>
2023-10-12 06:01:54 -0700

    Modified.

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,15 +11,16 @@ void print_triangle(int size)
 		_putchar('\n');
 	} else
 	{
-
-		for (int i = 1; i <= size; i++)
+	  int i,j;
+	  
+		for (i = 1; i <= size; i++)
 		{
-			for (int j = i; j < size; j++)
+			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
 
-			for (int j = 1; j <= i; j++)
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
